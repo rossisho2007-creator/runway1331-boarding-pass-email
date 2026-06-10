2270a7515bf7b0e3174262e087cf5fa245aebf3e
Rossisho2007 <rossisho2007@gmail.com>
2026-06-10 12:47:32 +0000

    style: change squawk code box background to soft blue-gray

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /**
  * Runway1331 Boarding Pass System
- * Main entry point - generates and sends boarding pass emails
+ * Main entry point
  */
 
 const EmailService = require('./email-service/sendEmail');
@@ -11,30 +11,19 @@ const TemplateEngine = require('./email-service/templateEngine');
 class BoardingPassSystem {
     
     constructor(config = {}) {
-        this.emailService = new EmailService(config);
+        this.emailService = new EmailService({
+            logoUrl: config.logoUrl || 'https://runway1331.com/logo.png'
+        });
         this.gifGenerator = new GifGenerator();
         this.templateEngine = new TemplateEngine();
         this.gifBaseUrl = config.gifBaseUrl || 'https://runway1331.com/gifs';
     }
 
-    /**
-     * Process a single guest: generate PIN, map building to zone, build email
-     * @param {object} guest - Guest data
-     * @param {string} guest.email
-     * @param {string} guest.name
-     * @param {number} guest.buildingNumber
-     * @param {string} guest.checkInDate
-     * @returns {object} Complete email ready to send
-     */
     processGuest(guest) {
         
-        // Generate PIN if not provided
         const pin = guest.pin || SquawkCodeFormatter.generatePin();
-        
-        // Map building to zone GIF
         const gif = this.gifGenerator.getGifForBuilding(guest.buildingNumber);
         
-        // Build email payload
         const email = this.emailService.buildBoardingPassEmail({
             email: guest.email,
             name: guest.name,
@@ -45,26 +34,15 @@ class BoardingPassSystem {
             zone: gif.zone
         });
         
-        // Render HTML template
         email.html = this.templateEngine.render('boarding-pass', email.templateVars);
         
         return email;
     }
 
-    /**
-     * Process multiple guests in batch
-     * @param {array} guests - Array of guest objects
-     * @returns {array} Array of email payloads
-     */
     processBatch(guests) {
         return guests.map(guest => this.processGuest(guest));
     }
 
-    /**
-     * Process and send to a single guest
-     * @param {object} guest - Guest data
-     * @returns {object} Send result
-     */
     async processAndSend(guest) {
         const email = this.processGuest(guest);
         return this.emailService.send(email);
